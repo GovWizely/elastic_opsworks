cc73d06051e3f112c854b93346d8dbb4c16f9b7e
Kerits and Letus <kerits.n.letus@gmail.com>
2018-04-03 14:39:56 -0400

    [EDSP-383] Upgrade elasticsearch to 5.6.8

--- metadata.rb
+++ metadata.rb
@@ -7,5 +7,5 @@ long_description 'Installs/Configures elastic_opsworks'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
-depends 'elasticsearch'
+depends 'elasticsearch', '< 4.0'
 depends 'java'
