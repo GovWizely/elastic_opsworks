bd7fcaf99632123a0fd1eadf588470146b97434f
James Isaacs <james@govwizely.com>
2018-12-03 07:42:11 -0500

    Update kibana_setup to start logstash using new name logstash_setup

--- recipes/kibana_setup.rb
+++ recipes/kibana_setup.rb
@@ -6,7 +6,7 @@
 
 node.default['elastic_opsworks']['elasticsearch']['plugins'] = node['elastic_opsworks']['elasticsearch']['plugins']
 include_recipe 'elastic_opsworks::elasticsearch_setup'
-include_recipe 'elastic_opsworks::logstash'
+include_recipe 'elastic_opsworks::logstash_setup'
 
 elastic_install 'kibana' do
   version node['elastic_opsworks']['kibana']['version']
