b13dcf317b9c6c2e8be1a57b4730159fe7e4badd
James Isaacs <james@govwizely.com>
2018-12-07 02:22:16 -0500

    remove credentials file creation for sync

--- recipes/logstash_sync_config.rb
+++ recipes/logstash_sync_config.rb
@@ -4,22 +4,7 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-directory '/home/ec2-user/.aws' do
-  owner 'ec2-user'
-  group 'ec2-user'
-  mode '0755'
-  action :create
-end
-
 application_hash = search(:aws_opsworks_app, 'shortname:elasticsearch').first.to_hash
-
-file '/home/ec2-user/.aws/credentials' do
-  not_if { ::File.exist?('/home/ec2-user/.aws/config') }
-  content "[default]
-aws_access_key_id = #{application_hash['environment']['aws_access_key']}
-aws_secret_access_key = #{application_hash['environment']['aws_secret_key']}
-"
-end
 
 execute 'sync_config' do
   command 'aws s3 sync s3://handle-logs-config /home/ec2-user/logstash_config'
