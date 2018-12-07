d9b696ed51f38a7eccd4cc3f77fd1c7fa546b833
James Isaacs <james@govwizely.com>
2018-12-07 02:18:43 -0500

    sync config dir

--- recipes/logstash_sync_config.rb
+++ recipes/logstash_sync_config.rb
@@ -22,7 +22,7 @@ aws_secret_access_key = #{application_hash['environment']['aws_secret_key']}
 end
 
 execute 'sync_config' do
-  command 'aws s3 sync s3://handle-logs-config logstash_config'
+  command 'aws s3 sync s3://handle-logs-config /home/ec2-user/logstash_config'
   environment ({
     'AWS_ACCESS_KEY_ID' => "#{application_hash['environment']['aws_access_key']}",
     'AWS_SECRET_ACCESS_KEY' => "#{application_hash['environment']['aws_secret_key']}"
