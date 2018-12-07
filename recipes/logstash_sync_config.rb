f6031e13ad5754d3fabeddad867470606f8c6b52
James Isaacs <james@govwizely.com>
2018-12-07 11:20:51 -0500

    Change s3 bucket name & recipe info

--- recipes/logstash_sync_config.rb
+++ recipes/logstash_sync_config.rb
@@ -1,13 +1,13 @@
 #
 # Cookbook:: elastic_opsworks
-# Recipe:: logstash_restart
+# Recipe:: logstash_sync_config
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 application_hash = search(:aws_opsworks_app, 'shortname:elasticsearch').first.to_hash
 
 execute 'sync_config' do
-  command 'aws s3 sync s3://dlrep-elastic-rawdata /etc/logstash/conf.d/'
+  command 'aws s3 sync s3://dlrep-elastic-logstashconfig /etc/logstash/conf.d/'
   environment ({
     'AWS_ACCESS_KEY_ID' => "#{application_hash['environment']['aws_access_key']}",
     'AWS_SECRET_ACCESS_KEY' => "#{application_hash['environment']['aws_secret_key']}"
