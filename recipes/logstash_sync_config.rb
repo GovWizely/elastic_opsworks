a4b292922cce430a0d226d608cb98a260ef16815
James Isaacs <james@govwizely.com>
2018-12-06 22:21:11 -0500

    aws cli grab app hash

--- recipes/logstash_sync_config.rb
+++ recipes/logstash_sync_config.rb
@@ -11,10 +11,12 @@ directory '/home/ec2-user/.aws' do
   action :create
 end
 
+application_hash = search(:aws_opsworks_app, 'shortname:elasticsearch').first.to_hash
+
 file '/home/ec2-user/.aws/credentials' do
   not_if { ::File.exist?('/home/ec2-user/.aws/config') }
   content "[default]
-aws_access_key_id = #{application_hash['environment']['aws_access_key_id']}
+aws_access_key_id = #{application_hash['environment']['aws_access_key']}
 aws_secret_access_key = #{application_hash['environment']['aws_secret_key']}
-          "
+"
 end
