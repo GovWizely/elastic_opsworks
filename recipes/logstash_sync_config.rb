51df284c4d8e3c9719e6460034f02f4ca28d80af
James Isaacs <james@govwizely.com>
2018-12-06 22:37:00 -0500

    sync config dir

--- recipes/logstash_sync_config.rb
+++ recipes/logstash_sync_config.rb
@@ -4,19 +4,13 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-directory '/home/ec2-user/.aws' do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
-end
-
 application_hash = search(:aws_opsworks_app, 'shortname:elasticsearch').first.to_hash
 
-file '/home/ec2-user/.aws/credentials' do
-  not_if { ::File.exist?('/home/ec2-user/.aws/config') }
-  content "[default]
-aws_access_key_id = #{application_hash['environment']['aws_access_key']}
-aws_secret_access_key = #{application_hash['environment']['aws_secret_key']}
-"
+execute 'sync_config' do
+  environment ({
+    "AWS_ACCESS_KEY_ID" => "#{application_hash['environment']['aws_access_key']}",
+    "AWS_SECRET_ACCESS_KEY" => "#{application_hash['environment']['aws_secret_key']}"
+  })
+  command 'aws s3 sync s3://handle-logs-config logstash_config'
+  action :run
 end
