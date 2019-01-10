4cb9c21dacef17cd116a1b2749608293122f4384
James Isaacs <james@govwizely.com>
2019-01-10 11:54:31 -0500

    Bump version

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,12 +4,12 @@ default['elastic_opsworks']['elasticsearch']['cluster.name'] = nil
 default['elastic_opsworks']['elasticsearch']['custom_configuration'] = {}
 default['elastic_opsworks']['elasticsearch']['network.host'] = '_ec2_'
 default['elastic_opsworks']['elasticsearch']['plugins'] = %w(discovery-ec2 repository-s3)
-default['elastic_opsworks']['elasticsearch']['version'] = '6.4.2'
+default['elastic_opsworks']['elasticsearch']['version'] = '6.5.4'
 default['elastic_opsworks']['elasticsearch']['zen.minimum_master_nodes'] = 2
 
 default['elastic_opsworks']['kibana']['custom_configuration'] = {}
 default['elastic_opsworks']['kibana']['indices'] = %w(.kibana)
-default['elastic_opsworks']['kibana']['version'] = '6.4.2'
+default['elastic_opsworks']['kibana']['version'] = '6.5.4'
 
 default['elastic_opsworks']['xpack']['enabled'] = true
 default['elastic_opsworks']['xpack']['indices'] = %w(.security .monitoring* .watches .triggered_watches .watcher-history*)
