0353dc721958d1ec974c18914b93a2b8b7bbf447
James Isaacs <james@govwizely.com>
2019-02-12 15:37:44 -0500

    Bring down elastic version to 6.5.4. The recipes haven't been updated to 6.6 yet.

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@ default['elastic_opsworks']['elasticsearch']['cluster.name'] = nil
 default['elastic_opsworks']['elasticsearch']['custom_configuration'] = {}
 default['elastic_opsworks']['elasticsearch']['network.host'] = '_ec2_'
 default['elastic_opsworks']['elasticsearch']['plugins'] = %w(discovery-ec2 repository-s3)
-default['elastic_opsworks']['elasticsearch']['version'] = '6.6'
+default['elastic_opsworks']['elasticsearch']['version'] = '6.5.4'
 default['elastic_opsworks']['elasticsearch']['zen.minimum_master_nodes'] = 2
 
 default['elastic_opsworks']['kibana']['custom_configuration'] = {}
