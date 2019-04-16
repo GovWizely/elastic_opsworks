18acc8ff964fbb67e966e2cabd006ffc43f4bba7
Kerits and Letus <kerits.n.letus@gmail.com>
2019-04-16 15:47:57 -0400

    Downloading JDK directly from Oracle is unreliable

--- attributes/java.rb
+++ attributes/java.rb
@@ -1,3 +1,5 @@
 default['java']['install_flavor'] = 'oracle'
 default['java']['jdk_version'] = '8'
 default['java']['oracle']['accept_oracle_download_terms'] = true
+default['java']['jdk']['8']['x86_64']['url'] = 'https://s3.amazonaws.com/cookbook-binaries/java/jdk-8u201-linux-x64.tar.gz'
+default['java']['jdk']['8']['x86_64']['checksum'] = 'cb700cc0ac3ddc728a567c350881ce7e25118eaf7ca97ca9705d4580c506e370'
